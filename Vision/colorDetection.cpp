f61ecaa0d04fdaa0e2405d741c005fe3c704375b
Jochem van Kranenburg <43204141+JVKran@users.noreply.github.com>
2019-04-03 10:44:58 +0200

    Create colorDetection.cpp
    Voorwerpen worden gedetecteerd op basis van kleur.

---
+++ Vision/colorDetection.cpp
@@ -0,0 +1,82 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h> 
+
+
+using namespace cv;
+using namespace std;
+
+
+int ct = 0; 
+char tipka;
+char filename[100]; // For filename
+int  c = 1; // For filename
+
+Mat redFilter(const Mat& src){
+    assert(src.type() == CV_8UC3);
+
+    Mat redOnly;
+    inRange(src, Scalar(0, 0, 0), Scalar(125, 125, 255), redOnly);
+
+    return redOnly;
+}
+
+int main(int, char**){
+
+
+    Mat frame;
+    //--- INITIALIZE VIDEOCAPTURE
+    VideoCapture cap;
+    // open the default camera using default API
+    cap.open(0);
+    // check if we succeeded
+    if (!cap.isOpened()) {
+        cerr << "ERROR! Unable to open camera\n";
+        return -1;
+    }
+    //--- GRAB AND WRITE LOOP
+    cout << "Start grabbing" << endl
+        << "Press a to terminate" << endl;
+    for (;;)
+    {
+        // wait for a new frame from camera and store it into 'frame'
+        cap.read(frame);
+
+        if (frame.empty()) {
+            cerr << "ERROR! blank frame grabbed\n";
+            break;
+        }
+
+
+        usleep(500); // Sleep is mandatory - for no leg!
+        Mat redOnly = redFilter(frame);
+
+
+        // show live and wait for a key with timeout long enough to show images
+        imshow("CAMERA 1", frame);  // Window name
+        imshow("Red Camera", redOnly);  // Window name
+
+
+        tipka = cv::waitKey(30);
+
+	if(tipka == 'q'){
+            cv::waitKey(10); 
+
+            imshow("CAMERA 1", frame);
+            imwrite("test.jpg", frame);
+            cout << "Frame_" << c << endl;
+            c++;
+	}
+
+        if (tipka == 'a') {
+            cout << "Terminating..." << endl;
+            usleep(2000000);
+            break;
+        }
+
+
+    }
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
+}
