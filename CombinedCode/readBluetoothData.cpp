43182124661573f42e9c73f72d0c0702739dec6a
Jochem van Kranenburg <43204141+JVKran@users.noreply.github.com>
2019-04-05 14:59:29 +0200

    Lees interpreteerbare gegevens

--- CombinedCode/readBluetoothData.cpp
+++ CombinedCode/readBluetoothData.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 #include <unistd.h>
 #include <fstream>
-
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
 int main () {
   string line;
+  vector<string> y(3);
+  int j = 0;
+  string tmp;
   ifstream myfile ("/dev/rfcomm0");
   if (myfile.is_open()){
     while ( getline (myfile,line) ){
-      cout << line << endl;
+      tmp = "";
+      j = 0;
+      for(unsigned int i = 0; i < line.size(); i++){
+	if(line[i] != ','){
+            tmp += line[i];
+        }
+        if(line[i] == ',' || line.size()-1 == i){
+            y[j] = tmp;
+	    j++;
+            tmp = "";
+       	}
+      }
+      cout << "1: " << y[0] << " 2: " << y[1] << " 3: " << y[2] << endl;
     }
   } else {
       cout << "Unable to open file"; 
