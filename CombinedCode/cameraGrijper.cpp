ada27d097d35ab6bce47ea4630994b2e2eebdb04
FaizalSupriadi <43231255+FaizalSupriadi@users.noreply.github.com>
2019-04-05 11:27:30 +0200

    Update cameraGrijper.cpp

--- CombinedCode/cameraGrijper.cpp
+++ CombinedCode/cameraGrijper.cpp
@@ -5,6 +5,7 @@
 #include "BrickPi3.h"
 #include <signal.h>
 #include <iomanip>
+#include "BluetoothSocket.h"
 
 
 using namespace cv;
@@ -33,38 +34,21 @@ void fwd(int speed=45){
     BP.set_motor_power(PORT_B, speed*1.07); //ivm ongelijkheid motoren.
     BP.set_motor_power(PORT_C, speed);
 }
+void fwdDPS(int speed=45){
+    BP.set_motor_dps(PORT_B, speed); //ivm ongelijkheid motoren.
+    BP.set_motor_dps(PORT_C, speed);
+}
+void down(int speed=-45){
+	BP.set_motor_dps(PORT_B, speed); //ivm ongelijkheid motoren.
+    	BP.set_motor_dps(PORT_C, speed);
+}
 
 void stop(void){
 	BP.set_motor_power(PORT_B, 0);
     	BP.set_motor_power(PORT_C, 0);
 }
 
-void detect(sensor_ultrasonic_t Ultrasonic2){
-	BP.set_motor_position(PORT_D, -4);		//De klauwen gaan open
-	cout << "De klauwen worden nu geopend!" << endl;
 
-	while(true){
-		usleep(50000);
-
-		if(BP.get_sensor(PORT_2, Ultrasonic2) == 0 && Ultrasonic2.cm < 50){
-			cout << "Cm: " << Ultrasonic2.cm << endl;
-			
-			if(BP.get_sensor(PORT_2, Ultrasonic2) == 0 && Ultrasonic2.cm < 8){
-				stop();
-				BP.set_motor_power(PORT_D, 20);
-				break;
-			}
-			else{
-				fwd(500);
-			}
- 		}else if(BP.get_sensor(PORT_2, Ultrasonic2) == 0 && Ultrasonic2.cm > 49 || Ultrasonic2.cm < 0){
-			break;
-		}
-		else{
-			BP.set_motor_position(PORT_D, -4);
-		}
-	}
-}
 
 bool initialize(){
 	if(BP.get_voltage_battery() < 10.85){
@@ -103,6 +87,63 @@ void manualDirection(int left=15, int right=15){
     BP.set_motor_dps(PORT_C, right);
 }
 
+void detect(sensor_ultrasonic_t Ultrasonic2){
+	BP.set_motor_position(PORT_D, -4);		//De klauwen gaan open
+	cout << "De klauwen worden nu geopend!" << endl;
+
+	while(true){
+		usleep(50000);
+
+		if(BP.get_sensor(PORT_2, Ultrasonic2) == 0 && Ultrasonic2.cm < 50){
+			cout << "Cm: " << Ultrasonic2.cm << endl;
+			
+			if(BP.get_sensor(PORT_2, Ultrasonic2) == 0 && Ultrasonic2.cm < 8){
+				stop();
+				BP.set_motor_power(PORT_D, 20);
+				break;
+			}
+			else{
+				fwd(500);
+			}
+ 		}else if(BP.get_sensor(PORT_2, Ultrasonic2) == 0 && Ultrasonic2.cm > 49 || Ultrasonic2.cm < 0){
+			break;
+		}
+		else{
+			BP.set_motor_position(PORT_D, -4);
+		}
+	}
+}
+
+void moreSpeed(int & IamSpeed,int & bocht){
+	if(IamSpeed > 50 && bocht > 150){
+		cout<< "Sloom" << endl;
+		IamSpeed = 500;
+		bocht = 150;
+	}else{
+		cout<< "I AM SPEED" << endl;
+		IamSpeed = 1000;
+		bocht = 500;
+	}
+}
+
+void control(sensor_ultrasonic_t Ultrasonic2, BluetoothSocket* clientsock){
+		string input;
+	while(true){
+		usleep(50000);
+		cout << "Cm: " << Ultrasonic2.cm << endl;
+		MessageBox& mb = clientsock->getMessageBox();
+		input = mb.readMessage();
+		if(input != ""){
+			break;
+		}
+		if(BP.get_sensor(PORT_2, Ultrasonic2) == 0 && Ultrasonic2.cm < 20){
+			
+			stop();
+			break;
+		}
+	}
+}
+
 int main(int, char**){
     initialize();
     sensor_color_t      Color1;
@@ -110,6 +151,8 @@ int main(int, char**){
     sensor_light_t      Light3;
     sensor_touch_t      Touch4;
     string input;
+    int IAmSpeed=500;
+    int bocht = 150;
     
     Mat frame;
     //--- INITIALIZE VIDEOCAPTURE
@@ -126,57 +169,93 @@ int main(int, char**){
         << "Press a to terminate" << endl;
     for (;;)
     {
-	cout << "Type c(voor camera) of g(voor grijpen): "<<endl;
-	cin >> input;
-	if(input=="c"){
-	for(;;){
-        // wait for a new frame from camera and store it into 'frame'
-        cap.read(frame);
-
-        if (frame.empty()) {
-            cerr << "ERROR! blank frame grabbed\n";
-            break;
-        }
-
-
-        usleep(500); // Sleep is mandatory - for no leg!
-        Mat redOnly = redFilter(frame);
-
-
-        // show live and wait for a key with timeout long enough to show images
-        imshow("CAMERA 1", frame);  // Window name
-        imshow("Red Camera", redOnly);  // Window name
-	if(cv::countNonZero(redOnly) > 50000 || (BP.get_sensor(PORT_2, Ultrasonic2) == 0 && Ultrasonic2.cm < 15)){
-		cv::Mat left = redOnly(cv::Range(0, redOnly.rows -1), cv::Range(0, redOnly.cols / 2 -1));
-		cv::Mat right = redOnly(cv::Range(0, redOnly.rows -1), cv::Range(redOnly.cols / 2 + 1, redOnly.cols -1));
-		int rightWhite = cv::countNonZero(right);
-		int leftWhite = cv::countNonZero(left);
-		if(leftWhite > rightWhite){
-			cout << "Turn Right!" << endl;
-			solidRight(200);
-		} else {
-			cout << "Turn Left!" << endl;
-			solidLeft(200);
-		}
+	BluetoothServerSocket serversock(2, 1);  //2 is het channel-number
+	cout << "listening" << endl;
+	while(true) {
+		BluetoothSocket* clientsock = serversock.accept();
+		cout << "accepted from " << clientsock->getForeignAddress().getAddress() << endl;
+		MessageBox& mb = clientsock->getMessageBox();
+			
 		
-	}else if(BP.get_sensor(PORT_2, Ultrasonic2) == 0 && Ultrasonic2.cm <= 10){
-		stop();
-		break;
-	}
+		while(mb.isRunning()) {
+			cout << "A(voor camera) of B(voor grijpen): "<<endl;
+			input = mb.readMessage();  //blokkeert niet
+			if(input != ""){
+			cout <<endl<<"input: " << input << endl;
+			}
 
-	else {
-		fwd();
-	}
-	tipka = cv::waitKey(30);
+			if(input=="A"){
+			for(;;){
+        		// wait for a new frame from camera and store it into 'frame'
+        		cap.read(frame);
+
+        		if (frame.empty()) {
+           			cerr << "ERROR! blank frame grabbed\n";
+            			break;
+        		}
+
+
+        		usleep(500); // Sleep is mandatory - for no leg!
+        		Mat redOnly = redFilter(frame);
+
+
+        		// show live and wait for a key with timeout long enough to show images
+        		imshow("CAMERA 1", frame);  // Window name
+        		imshow("Red Camera", redOnly);  // Window name
+			if(cv::countNonZero(redOnly) > 50000 || (BP.get_sensor(PORT_2, Ultrasonic2) == 0 && Ultrasonic2.cm < 15)){
+				cv::Mat left = redOnly(cv::Range(0, redOnly.rows -1), cv::Range(0, redOnly.cols / 2 -1));
+				cv::Mat right = redOnly(cv::Range(0, redOnly.rows -1), cv::Range(redOnly.cols / 2 + 1, redOnly.cols -1));
+				int rightWhite = cv::countNonZero(right);
+				int leftWhite = cv::countNonZero(left);
+				if(leftWhite > rightWhite){
+					cout << "Turn Right!" << endl;
+					solidRight(200);
+				} else {
+					cout << "Turn Left!" << endl;
+					solidLeft(200);
+				}
+		
+			}else if(BP.get_sensor(PORT_2, Ultrasonic2) == 0 && Ultrasonic2.cm <= 10){
+				stop();
+				break;
+			}
+
+			else {
+				fwd();
+			}
+			tipka = cv::waitKey(30);
 	
-    }
-}
-else if(input=="g"){
-	detect(Ultrasonic2);
-}
-    // the camera will be deinitialized automatically in VideoCapture destructor
-    return 0;
- }
+    		}
+	}
+		else if(input=="B"){
+			detect(Ultrasonic2);
+		}
+		else if(input=="C"){
+			moreSpeed(IAmSpeed, bocht);
+		}
+		else if(input == "UP"){ 
+			fwdDPS(IAmSpeed);
+			control(Ultrasonic2,clientsock);
+		}else if(input == "LEFT"){
+			solidLeft(bocht);
+		}else if(input == "RIGHT"){
+			solidRight(bocht);
+		}else if(input == "DOWN"){
+			down(-IAmSpeed);
+		}else if(input == "FIRE"){
+			stop();
+		}
+		//doe andere dingen.
+		cout << ".";
+		cout.flush();
+		usleep(500000); // wacht 500 ms
+		}	
+		clientsock->close();
+   		 // the camera will be deinitialized automatically in VideoCapture destructor
+    		return 0;
+			
+		}
+	}
 }
 
 void exit_signal_handler(int signo){
