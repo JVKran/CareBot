315368d275540000302d0b2017a7ee0ded10854e
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-04-12 18:26:55 +0200

    commands
    duidelijk gemaakt wat elk stukje code doet en waarom sommige dingen in de code zo zijn zoals ze zijn.

--- Resultaat/manControl.cpp
+++ Resultaat/manControl.cpp
@@ -8,9 +8,9 @@ using namespace std;
 
 BrickPi3 BP;
 
-bool grab = false;
 void exit_signal_handler(int signo);
 
+//functie die er voor zorgt dat de motoren die bedoeld zijn om te rijden worden uitgezet
 void stop(void){
 	BP.set_motor_power(PORT_B, 0);
     	BP.set_motor_power(PORT_C, 0);
@@ -29,8 +29,9 @@ bool initialize(){
 	BP.set_motor_limits(PORT_C, 100, 0);
 }
 
+//bepaald per motor de dps
 void manualDirection(int left=15, int right=15){
-    BP.set_motor_dps(PORT_B, left*1.07);
+    BP.set_motor_dps(PORT_B, left*1.07);		//1*07 in verband met ongelijkheid motoren
     BP.set_motor_dps(PORT_C, right);
 }
 
@@ -46,51 +47,53 @@ int main () {
 	exit(-2);
   }
 
-  string line;
-  vector<string> y(3);
-  int j = 0;
-  bool grab = false;
-  bool odd = true;
-  string tmp;
-  ifstream myfile ("/dev/rfcomm0");
-  if (myfile.is_open()){
-    while ( getline (myfile,line) ){
-      tmp = "";
-      j = 0;
-      if(line.size() == 1){
-	  exit(stoi(line)); 
+  string line;					//hierin wordt de output van wat er in /dev/rfcomm0 staat opgeslagen
+  vector<string> y(3);				//hierin worden de dingen die opgedeelt zijn van /dev/rfcomm0 opgeslagen voor verder gebruik
+  int j = 0;					//houd bij waar in de vector een waarde moet worden toegevoegd
+  bool grab = false;				//houd bij of de grijper open is of niet
+  bool odd = true;				//zorgt er voor dat de dubbele invoer bij /dev/rfcomm0 de grijper niet open en meteen weer dicht doet
+  string tmp;					//houd de verdeelde waardes tijdelijk vast
+  
+  ifstream myfile ("/dev/rfcomm0");				//opent /dev/rfcomm0
+  if (myfile.is_open()){					//checked of het geopend is
+    while ( getline (myfile,line) ){				//gaat door alle zinnen in /dev/rfcomm0
+      tmp = "";							//zorgt er voor dat tmp weer leeg is
+      j = 0;							//reset j zodat het opnieuw gebruikt kan worden
+      if(line.size() == 1){					
+	  exit(stoi(line)); 					
       }
-      for(unsigned int i = 0; i < line.size(); i++){
-	if(line[i] != ','){
-            tmp += line[i];
+      for(unsigned int i = 0; i < line.size(); i++){		//loopt door alle karakters uit de string line
+	if(line[i] != ','){					//checked of het karakter niet gelijk is aan een komma zodat het toegevoegd kan worden aan tmp
+            tmp += line[i];					//voegt het karakter toe
         }
-        if(line[i] == ',' || line.size()-1 == i){
-            y[j] = tmp;
-	    j++;
-            tmp = "";
+        if(line[i] == ',' || line.size()-1 == i){		//checked of er een komma gevonden is of als het de laatste karakter is
+            y[j] = tmp;						//tmp wordt op de juiste locatie gezet in de vector
+	    j++;						//j wordt een hoger zodat het volgende element in tmp op de juiste plek in de vector wordt toegevoegd
+            tmp = "";						//tmp wordt weer leeg gemaakt zodat het volgende element er in past
        	}
       }
+	    
+      //checked of /dev/rfcomm0 bepaalde waardes geeft die hoger of kleiner zijn dan een bepaalde waarde om te voorkomen dat de robot automatisch naar achteren rijd
       if(stoi(y[0]) > 2000 || stoi(y[0]) < 1600 || stoi(y[1]) > 2000 || stoi(y[1]) < 1600){
+	//berekening om te bepalen hoeveel dps de motoren krijgen
       	manualDirection((((stoi(y[0])-2000)/2)-((stoi(y[1])-2048)/4)), (((stoi(y[0])-2000)/2)+((stoi(y[1])-2048)/4)));
       } else {
-	stop();
+	stop();						//stopt de motoren
       }
 
-      if(stoi(y[2])==0 && !grab && odd){
-	//De button geeft twee keer 0 mee met 1 keer drukken
-	BP.set_motor_power(PORT_D, 0);
-	BP.set_motor_position(PORT_D, -4);
-	grab = true;
-	odd = false;
+      if(stoi(y[2])==0 && !grab && odd){		//checked of de grijper dicht is en zorgt er dan voor dat de grijper dicht gaat
+	BP.set_motor_power(PORT_D, 0);			//stopt de motor		
+	BP.set_motor_position(PORT_D, -4);		//zorgt er voor dat de grijper opgen gaat
+	grab = true;					//verander de waarde van grab zodat de robot weet dat de grijper open is
+	odd = false;					//zorgt er voor dat de loop een keer niks doet met de grijper in verband met dubbele input voor grijper
 	cout << "open\n";
-      }else if(stoi(y[2])==0 && grab && odd){
-	
-	BP.set_motor_power(PORT_D, 20);
-	grab = false;
-        odd = false;
+      }else if(stoi(y[2])==0 && grab && odd){		//checked of de grijper open is en zorgt er dan voor dat de grijper dicht gaat
+	BP.set_motor_power(PORT_D, 20);			//zet de motor power op 20 om er voor te zorgen dat de grijper grip blijft houden op het object
+	grab = false;					//verander de waarde van grab zodat de robot weet dat de grijper dicht is
+        odd = false;					//zorgt er voor dat de loop een keer niks doet met de grijper in verband met dubbele input voor grijper
 	cout << "dicht\n";
       }else{
- 	odd = true;
+ 	odd = true;					//na een overgeslagen rondje in de loop zorgt dit er voor dat de grijper weer gebruikt kan worden
     }
     }
   } else {
