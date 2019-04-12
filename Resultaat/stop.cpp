2f651a4525a6a19862ecc12e144d34657e60a674
1122joshua1 <43231303+1122joshua1@users.noreply.github.com>
2019-04-12 18:31:02 +0200

    Update stop.cpp

--- Resultaat/stop.cpp
+++ Resultaat/stop.cpp
@@ -1,4 +1,5 @@
-
+//bestaat omdat niet alle programma's de motoren goed stoppen, vooral na een crash, dit programma kan makkelijk aangeroepen worden
+//om de motoren te stoppen
 
 #include "BrickPi3.h" // for BrickPi3
 #include <stdio.h>      // for printf
@@ -7,7 +8,6 @@
 
 BrickPi3 BP;
 
-void exit_signal_handler(int signo);
 
 int main(){
     BP.reset_all();    // Reset everything so there are no run-away motors
