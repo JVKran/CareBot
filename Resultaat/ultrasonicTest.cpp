36192560f51dafe382ae24e878c131b915730d34
FaizalSupriadi <43231255+FaizalSupriadi@users.noreply.github.com>
2019-04-12 19:48:25 +0200

    Update ultrasonicTest.cpp
    Hoofdletters 👍

--- Resultaat/ultrasonicTest.cpp
+++ Resultaat/ultrasonicTest.cpp
@@ -50,7 +50,7 @@ int main(){
 // Signal handler that will be called when Ctrl+C is pressed to stop the program
 void exit_signal_handler(int signo){
     if(signo == SIGINT){
-        BP.reset_all();    // Reset everything so there are no run-away motors
+        BP.reset_all();    //Reset everything so there are no run-away motors
         exit(-2);
     }
 }
